8e1b8482ed1eada2e0d3ae6051e29b283c49ab96
oDs1337 <tobias.contact@gmail.com>
2019-11-30 13:18:20 +0100

    Added filesystem support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,13 @@
  */
 
 module.exports = {
-
-  plugins: [`gatsby-plugin-netlify-cms`]
-  /* Your site config here */
+  plugins: [`gatsby-plugin-netlify-cms`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/content/blog`,
+    },
+  },
+  `gatsby-transformer-remark`,]
 }
